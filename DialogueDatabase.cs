0a6971fd168967bd234df5118929f639480509c1
Piotr Kołodziejczyk <gradir@gmail.com>
2026-01-11 23:03:30 +0100

    Update DialogueDatabase.cs

--- DialogueDatabase.cs
+++ DialogueDatabase.cs
@@ -1,25 +1,22 @@
 using UnityEditor;
 using UnityEngine;
 
-namespace Underneath
+[CreateAssetMenu(menuName = "Scriptable objects/Databases/Dialogue Database")]
+public class DialogueDatabase : ScriptableDatabase<Dialogue>
 {
-    [CreateAssetMenu(menuName = "Scriptable objects/Databases/Dialogue Database")]
-    public class DialogueDatabase : ScriptableDatabase<Dialogue>
+    #if UNITY_EDITOR
+    [ContextMenu("Fill")]
+    public void ContextFill()
     {
-        #if UNITY_EDITOR
-        [ContextMenu("Fill")]
-        public void ContextFill()
+        All.Clear();
+
+        var guids = AssetDatabase.FindAssets("t:" + nameof(Dialogue));
+
+        foreach (var iGuid in guids)
         {
-            All.Clear();
-
-            var guids = AssetDatabase.FindAssets("t:" + nameof(Dialogue));
-
-            foreach (var iGuid in guids)
-            {
-                var realPath = AssetDatabase.GUIDToAssetPath(iGuid);
-                All.Add((Dialogue)AssetDatabase.LoadAssetAtPath(realPath, typeof(Dialogue)));
-            }
+            var realPath = AssetDatabase.GUIDToAssetPath(iGuid);
+            All.Add((Dialogue)AssetDatabase.LoadAssetAtPath(realPath, typeof(Dialogue)));
         }
-        #endif
     }
+    #endif
 }
