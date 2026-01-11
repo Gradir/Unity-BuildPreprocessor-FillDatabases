c84c2180b4c80f0df5b42467c526a89a048fe339
Piotr Kołodziejczyk <gradir@gmail.com>
2026-01-11 23:03:01 +0100

    Update BuildPreprocessor.cs

--- BuildPreprocessor.cs
+++ BuildPreprocessor.cs
@@ -5,43 +5,40 @@ using UnityEditor.Build;
 using UnityEditor.Build.Reporting;
 using UnityEngine;
 
-namespace Underneath
+public class BuildPreprocessor : IPreprocessBuildWithReport
 {
-    public class BuildPreprocessor : IPreprocessBuildWithReport
+    public int callbackOrder => 0;
+	const string Fill = "Fill";
+	const string SearchPhrase = "t:scriptableObject database";
+
+    public void OnPreprocessBuild(BuildReport report)
     {
-        public int callbackOrder => 0;
-		    const string Fill = "Fill";
-		    const string SearchPhrase = "t:scriptableObject database";
+        FillDatabases();
+    }
+    
+    void FillDatabases()
+    {
+        var guids = AssetDatabase.FindAssets(SearchPhrase);
+        foreach (var guid in guids)
+        {
+            var path = AssetDatabase.GUIDToAssetPath(guid);
+            var asset = AssetDatabase.LoadMainAssetAtPath(path);
+            if (asset == null) continue;
 
-        public void OnPreprocessBuild(BuildReport report)
-        {
-            FillDatabases();
-        }
-        
-        void FillDatabases()
-        {
-            var guids = AssetDatabase.FindAssets(SearchPhrase);
-            foreach (var guid in guids)
+            var type = asset.GetType();
+            // Find the ScriptableDatabase<T> base type
+            var baseType = type;
+            while (baseType != null && (!baseType.IsGenericType || baseType.GetGenericTypeDefinition() != typeof(ScriptableDatabase<>)))
             {
-                var path = AssetDatabase.GUIDToAssetPath(guid);
-                var asset = AssetDatabase.LoadMainAssetAtPath(path);
-                if (asset == null) continue;
+                baseType = baseType.BaseType;
+            }
 
-                var type = asset.GetType();
-                // Find the ScriptableDatabase<T> base type
-                var baseType = type;
-                while (baseType != null && (!baseType.IsGenericType || baseType.GetGenericTypeDefinition() != typeof(ScriptableDatabase<>)))
-                {
-                    baseType = baseType.BaseType;
-                }
-
-                if (baseType != null)
-                {
-                    var itemType = baseType.GetGenericArguments()[0];
-                    var fillMethod = baseType.GetMethod(Fill, BindingFlags.Public | BindingFlags.Instance);
-                    fillMethod?.Invoke(asset, new object[] { itemType });
-                    Debug.Log($"Filled database: {asset.name} with type {itemType.Name}");
-                }
+            if (baseType != null)
+            {
+                var itemType = baseType.GetGenericArguments()[0];
+                var fillMethod = baseType.GetMethod(Fill, BindingFlags.Public | BindingFlags.Instance);
+                fillMethod?.Invoke(asset, new object[] { itemType });
+                Debug.Log($"Filled database: {asset.name} with type {itemType.Name}");
             }
         }
     }
